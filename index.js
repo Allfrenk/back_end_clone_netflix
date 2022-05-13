028e99b057163d236bb21982c410dc0953ab19de
Alessandro Catania <ale.cata.it@gmail.com>
2022-05-13 11:24:37 +0200

    aggiunto DISCOVER request

--- index.js
+++ index.js
@@ -3,21 +3,44 @@ const app = express();
 const port = 2000;
 const axios = require("axios").default;
 
-// KEY
-// 944143a810402073e91619e119fa95ef
+// VARIABILI UTILI
+const API_URL = "https://api.themoviedb.org/3/";
+const API_KEY = "944143a810402073e91619e119fa95ef";
+const language = "it";
 
+// DEFAULT ROUTE
 app.get("/", (req, res) => {
   res.send("Hello World!");
 });
 
+// DISCOVER
+app.get("/discover", (req, resp) => {
+  //
+  axios
+    .get(`${API_URL}discover/movie`, {
+      params: {
+        api_key: "944143a810402073e91619e119fa95ef",
+        language: "it",
+        sort_by: "popularity.desc",
+        include_adult: false,
+        page: 1,
+
+        //
+      },
+    })
+    .then((response) => {
+      const data = response.data.results;
+      resp.send(data);
+    });
+});
+
 app.get("/api/movie", (req, resp) => {
-  const language = "it";
   const movie = req.query.movie;
   const axios = require("axios").default;
   // richiesta api, movie e id
   axios
     .get(
-      `https://api.themoviedb.org/3/movie/550?api_key=944143a810402073e91619e119fa95ef&language=${language}`
+      `https://api.themoviedb.org/3/movie/550?api_key=${API_KEY}ef&language=${language}`
     )
     .then(function (response) {
       const movie = response.data;
