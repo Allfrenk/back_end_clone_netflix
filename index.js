1534bfd53efaf1af7b6c89055df7f9f52bcfcad1
Alessandro Catania <ale.cata.it@gmail.com>
2022-05-18 17:38:23 +0200

    aggiornato

--- index.js
+++ index.js
@@ -18,6 +18,8 @@ app.get("/", (req, res) => {
     discover_movie: "/discover/movie",
     discover_tv: "/discover/tv",
     search_multi: "/search/multi?query=",
+    discover_tv_genre: "/discover/tv/genre?genre=",
+    discover_movie_genre: "/discover/movie/genre?genre=",
   });
 });
 
@@ -75,6 +77,81 @@ app.get("/search/multi", (req, resp) => {
     .then((response) => {
       const results = response.data.results;
       resp.send(results);
+    });
+});
+
+// CERCA PER GENERE TV
+app.get("/discover/tv/genre", (req, resp) => {
+  const genre = req.query.genre.toLowerCase();
+
+  // let genres = new Map();
+  // genres.set("azione", "10759");
+  // genres.set("avventura", "10759");
+  // genres.set("animazione", "16");
+  // genres.set("commedia", "35");
+  // genres.set("crime", "80");
+  // genres.set("documentario", "99");
+  // genres.set("dramma", "18");
+  // genres.set("famiglia", "10751");
+  // genres.set("kids", "10762");
+  // genres.set("mistero", "9648");
+  // genres.set("news", "10763");
+  // genres.set("reality", "10764");
+  // genres.set("sci-fi", "10765");
+  // genres.set("fantasy", "10765");
+  // genres.set("soap", "10766");
+  // genres.set("talk", "10767");
+  // genres.set("war", "10768");
+  // genres.set("politics", "10768");
+  // genres.set("western", "37");
+
+  axios
+    .get(`${API_URL}discover/tv`, {
+      params: {
+        api_key: API_KEY_V3,
+        language: language,
+        sort_by: "popularity.desc",
+        page: 1,
+        with_genres: genre,
+        // with_genres: genres.get(genre),
+
+        include_null_first_air_dates: false,
+      },
+    })
+    .then((response) => {
+      const series = response.data.results;
+      resp.send(series);
+    })
+    .catch(function (error) {
+      console.log(error);
+    });
+});
+
+// CERCA PER GENERE MOVIE
+app.get("/discover/movie/genre", (req, resp) => {
+  const genre = req.query.genre.toLowerCase();
+
+  axios
+    .get(`${API_URL}discover/movie`, {
+      params: {
+        api_key: API_KEY_V3,
+        language: language,
+        region: region,
+        sort_by: "popularity.desc",
+        include_adult: false,
+        // include_video: false,
+        page: 1,
+        with_genres: genre,
+        include_null_first_air_dates: false,
+        // with_watch_monetization_types: filtrate,
+      },
+    })
+    .then((response) => {
+      const movies = response.data.results;
+      resp.send(movies);
+    })
+    .catch(function (error) {
+      console.log(error);
     });
 });
 
